21082125b6e1ad608c47fa244ab4341b82f2ce51
Karen Hsing <kxhsing@gmail.com>
2017-08-02 14:21:27 -0700

    Added reducer for posts, working on get request

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+componentDidMount() {
+    this.props.fetchPosts();
+}
+
     render() {
         return (
             <div>
@@ -8,7 +14,7 @@ class PostsIndex extends Component {
             </div>
         );
     }
-
 }
 
-export default PostsIndex;
+//there is null below as first argument because we aren't passing in mapStateToProps function
+export default connect(null, { fetchPosts })(PostsIndex);
